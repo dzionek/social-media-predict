de9264ce5baf13f3937d8e2601d3b0f5eef22a0a
dzionek <vburtoch@gmail.com>
2020-10-09 02:20:44 +0100

    Add the rest of unit tests for backend

--- prediction/main.py
+++ prediction/main.py
@@ -13,45 +13,45 @@ HEADERS = {
 
 class Prediction:
     def __init__(self, username: str, platform: str) -> None:
-        self.username = username
-        self.platform = platform
-        self.link = self._get_link()
-        self.content = self._get_content()
-        self.parsed: Optional[Dict] = None
+        self._username = username
+        self._platform = platform
+        self._link = self._get_link()
+        self._content = self._get_content()
+        self._parsed: Optional[Dict] = None
 
     def _get_link(self) -> str:
-        if self.platform == 'YouTube':
-            return f'https://socialblade.com/youtube/channel/{self.username}' \
+        if self._platform == 'YouTube':
+            return f'https://socialblade.com/youtube/channel/{self._username}'\
                    '/monthly'
-        elif self.platform == 'Twitch':
-            return f'https://socialblade.com/twitch/user/{self.username}' \
+        elif self._platform == 'Twitch':
+            return f'https://socialblade.com/twitch/user/{self._username}' \
                    '/monthly'
-        elif self.platform == 'Twitter':
-            return f'https://socialblade.com/twitter/user/{self.username}' \
+        elif self._platform == 'Twitter':
+            return f'https://socialblade.com/twitter/user/{self._username}' \
                    '/monthly'
         else:
-            raise ValueError('The given platform is not supported.')
+            raise NotImplementedError('The given platform is not supported.')
 
     def _get_content(self) -> str:
-        response = requests.get(self.link, headers=HEADERS)
+        response = requests.get(self._link, headers=HEADERS)
         return response.text
 
     def does_exist(self) -> bool:
-        return 'sorry' not in self.content
+        return 'sorry' not in self._content
 
     def get_parsed(self) -> Dict:
-        parser = Parser(self.content, self.platform)
-        self.parsed = parser.parse()
-        return self.parsed
+        parser = Parser(self._content, self._platform)
+        self._parsed = parser.parse()
+        return self._parsed
 
     def get_prediction(self) -> Dict:
-        if not self.parsed:
+        if not self._parsed:
             raise ChildProcessError('You need to parse first.')
         else:
-            assert isinstance(self.parsed, Dict)
+            assert isinstance(self._parsed, Dict)
 
-        dates_str = self.parsed['dates']
-        subscribers = self.parsed['subscribers']
+        dates_str = self._parsed['dates']
+        subscribers = self._parsed['subscribers']
 
         dates = [
             date(*map(int, date_str.split('-')))
@@ -62,14 +62,16 @@ class Prediction:
         dates_num = [(d - first_day).days for d in dates]
 
         prediction = self._linear_regression(
-            dates_num, subscribers, first_day, last_day_num=dates_num[-1]
+            dates_num, subscribers, first_day
         )
 
         return prediction
 
     @staticmethod
     def _linear_regression(x_values: List[int], y_values: List[int],
-                           first_day: date, last_day_num: int) -> Dict:
+                           first_day: date) -> Dict:
+
+        last_day_num = x_values[-1]
 
         slope, intercept, r, _, _ = stats.linregress(x_values, y_values)
 
